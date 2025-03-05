fa060acd2a57ecfd004dadbeaef29a2a2770cc93
Bartek Zych <bartekzych2222@gmail.com>
2025-03-05 14:59:17 +0100

    clean directory

--- layout.py
+++ layout.py
@@ -2,19 +2,12 @@ import tkinter
 import tkinter as tk
 from tkinter.ttk import Label
 from tkinter import font, Entry
+from canvas_rounded_corners import *
+import  inputText
 
 colors = {"darkerBlue" : "#6DAEC2",
           "lightnessBlue" : "#76C9D5",
           "grey" : "#D9D9D9"}
-
-def canvas_rounded_corners(radius, width, height, color, c:tk.Canvas):
-        diameter = radius * 2
-        c.create_oval(0, 0, diameter + 1, diameter + 1, fill=color, width=0)
-        c.create_oval(width - diameter, 0, width - 1, diameter - 1, fill=color, width=0)
-        c.create_oval(width - diameter, height - diameter, width - 1, height - 1, fill=color, width=0)
-        c.create_oval(0, height - diameter, diameter - 1, height - 1, fill=color, width=0)
-        c.create_polygon(radius, 0, width - radius, 0, width, radius, width, height - radius, width - radius, height,
-                         radius, height, 0, height - radius, 0, radius, fill=color)
 
 
 
