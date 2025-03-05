553d1fee6475cef09739596007ae1ed293e48d71
Bartek Zych <bartekzych2222@gmail.com>
2025-03-05 14:35:03 +0100

    end making class new widget, textinput with round corners

--- CustomInputText/inputText.py
+++ CustomInputText/inputText.py
@@ -1,46 +1,31 @@
-import tkinter as tk
-from tkinter import font, Frame, Entry
-from typing import  override
-import math as math
+from tkinter import Frame, Entry
+from canvas_rounded_corners import *
 
-def canvas_rounded_corners(radius, width, height, color, c: tk.Canvas):
-    diameter = radius * 2
-    c.create_oval(0, 0, diameter + 1, diameter + 1, fill=color, width=0)
-    c.create_oval(width - diameter, 0, width - 1, diameter - 1, fill=color, width=0)
-    c.create_oval(width - diameter, height - diameter, width - 1, height - 1, fill=color, width=0)
-    c.create_oval(0, height - diameter, diameter - 1, height - 1, fill=color, width=0)
-    c.create_polygon(radius, 0, width - radius, 0, width, radius, width, height - radius, width - radius, height,
-                     radius, height, 0, height - radius, 0, radius, fill=color)
+
 
 class InputText (Frame):
-    def __init__(self,parent,font,color='white'):
+    def __init__(self,parent,font,color='white',backgroundcolor='black',
+                 fontcolor='black',radius=0):
+
         tk.Frame.__init__(self,parent)
-        self.rowconfigure(0,weight=1)
-        self.columnconfigure(0,weight=1)
-        self.input = Entry(self,bg=color,font=font)
-        self.rounded = tk.Canvas(self,bg=color)
-        self.input.grid(row=0,column=0)
-        self.input.lift()
-        self.rounded.grid(row=0,column=0)
 
-root = tk.Tk()
-root.geometry("500x500")
-root.configure()
+        #create main property
+        self.radius=radius
+        self.color = color
 
-root.grid_columnconfigure((0,1,2), weight=1, uniform='a')
-root.grid_rowconfigure((0,1,2), weight=1, uniform='a')
+        #help value
+        x = radius-radius/pow(2,1/2)
 
-container1 = tk.Canvas(background='red')
-container2 = tk.Canvas(background='green')
-container3 = tk.Canvas(background='blue')
+        self.rowconfigure(0,weight=1,uniform='a')
+        self.columnconfigure(0,weight=1,uniform='a')
 
-container1.grid(row=0,column=0)
-container2.grid(row=1,column=1)
-container3.grid(row=2,column=2)
-root.update()
+        self.can = tk.Canvas(self,background=backgroundcolor)
+        self.inp = Entry(self,background=color,borderwidth=0,foreground=fontcolor, font = font)
 
-ipn = InputText(parent = root,font=font.Font(family="Times New Roman",size=8),color='black')
-ipn.grid(row=1,column=0)
+        #fill all, and add pad to corners
+        self.can.grid(row=0,column=0,sticky='nswe')
+        self.inp.grid(row=0,column=0,padx=x,pady=x,sticky='nswe')
 
-
-root.mainloop()
+    #function to add rounded corners to widget
+    def add_background (self,width,height):
+        canvas_rounded_corners(self.radius,width,height,self.color,self.can)
