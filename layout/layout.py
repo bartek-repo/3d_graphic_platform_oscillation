193db32b59f24cd0aba9f011a79fa0a87f234188
Bartek Zych <bartekzych2222@gmail.com>
2025-03-06 07:15:15 +0100

    reorganize all layout structure, because was some problem with grid

--- layout/layout.py
+++ layout/layout.py
@@ -1,172 +1,85 @@
-import tkinter
 import tkinter as tk
-from tkinter.ttk import Label
-from tkinter import font, Entry
+from tkinter import font
 
 colors = {"darkerBlue" : "#6DAEC2",
           "lightnessBlue" : "#76C9D5",
-          "grey" : "#D9D9D9"}
-
-def canvas_rounded_corners(radius, width, height, color, c:tk.Canvas):
-        diameter = radius * 2
-        c.create_oval(0, 0, diameter + 1, diameter + 1, fill=color, width=0)
-        c.create_oval(width - diameter, 0, width - 1, diameter - 1, fill=color, width=0)
-        c.create_oval(width - diameter, height - diameter, width - 1, height - 1, fill=color, width=0)
-        c.create_oval(0, height - diameter, diameter - 1, height - 1, fill=color, width=0)
-        c.create_polygon(radius, 0, width - radius, 0, width, radius, width, height - radius, width - radius, height,
-                         radius, height, 0, height - radius, 0, radius, fill=color)
-
-
+          "grey" : "#D9D9D9",
+          "white" : "#FFFFFF"}
 
 root = tk.Tk()
-root.geometry("500x300")
-root.title("Add Objects")
-root.configure()
+root.geometry("1000x500")
 
-root.columnconfigure((0,1), weight = 1, uniform = 'a')
-root.columnconfigure((2,3), weight = 2, uniform = 'a')
-root.rowconfigure(0,weight = 2, uniform = 'a')
-root.rowconfigure((1,2,3,4,5,6,7,8,9,10,11),weight = 1, uniform = 'a')
+fonts = {"buttonTopContainer" : font.Font(family="Times New Roman,Bold",size=15),
+         "bottomContainerTitle": font.Font(family="Times New Roman,Bold",size=15),
+         "bottomButtonFont": font.Font(family="Times New Roman,Bold",size=20)}
 
-fontContainer = font.Font(family="Times New Roman", size=8, weight="bold")
+root.rowconfigure(0, weight=2,uniform='a')
+root.rowconfigure((1,2,3,4,5,6,7,8,9,10,11,12,13),weight=1,uniform='a')
+root.columnconfigure((0,1,2,3,4,5),weight=1,uniform='b')
 
 #containers
-topContainer = tk.Canvas(background= colors["grey"],highlightthickness=0)
-bottomContainer = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-containerHandle = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-containerOriginTop = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-containerOriginBottom = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-containerMove = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-containerListObjects = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
 
-topContainer.grid(row = 0, column = 0,columnspan=4,sticky='nsew')
-bottomContainer.grid(row = 1, column = 0,columnspan=4,rowspan=11,sticky='nsew')
-containerHandle.grid(row = 1, column = 0, rowspan = 3,columnspan=2,padx = 5,pady=5)
-containerOriginTop.grid(row = 4, column = 0, rowspan = 3,columnspan=2,padx = 5,pady=5)
-containerOriginBottom.grid(row = 7, column = 0, rowspan = 3,columnspan=2,padx = 5,pady=5)
-containerMove.grid(row = 1, column = 2,rowspan = 11,padx=5, pady=5)
-containerListObjects.grid(row=1, column=3,rowspan=9, padx=5, pady=5)
-root.update()
+containerTop = tk.Canvas(root, bg=colors["grey"],highlightthickness=0)
+containerTop.grid(row=0,column=0,columnspan=6,sticky='nswe')
 
-#rounded corners containers
-canvas_rounded_corners(8,containerHandle.winfo_width(),containerHandle.winfo_height(),colors["lightnessBlue"], containerHandle)
-canvas_rounded_corners(8,containerOriginTop.winfo_width(),containerOriginTop.winfo_height(),colors["lightnessBlue"], containerOriginTop)
-canvas_rounded_corners(8,containerOriginBottom.winfo_width(),containerOriginBottom.winfo_height(),colors["lightnessBlue"], containerOriginBottom)
-canvas_rounded_corners(8,containerMove.winfo_width(),containerMove.winfo_height(),colors["lightnessBlue"], containerMove)
-canvas_rounded_corners(8,containerListObjects.winfo_width(),containerListObjects.winfo_height(),colors["lightnessBlue"], containerListObjects)
+containerButtonUpload = tk.Canvas(root,bg=colors["darkerBlue"],highlightthickness=0)
+containerButtonUpload.grid(row=0,column=0,padx=10,pady=10,sticky='nswe')
 
-#buttons
-uploadButton = tk.Canvas(background= colors["grey"],highlightthickness=0)
-textUploadButton = Label(text="upload",font=font.Font(family="Times New Roman", size=8, weight="bold"),background=colors["darkerBlue"],foreground='white')
-buttonPath = tk.Canvas(background= colors["grey"],highlightthickness=0)
-textButtonPath = Label(text="Path",font=font.Font(family="Times New Roman", size=8, weight="bold"),background=colors["darkerBlue"],foreground='white')
-buttonAdd = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-textButtonAdd = Label(text="add",font=font.Font(family="Times New Roman", size=16, weight="bold"),background=colors["lightnessBlue"],foreground='white')
-buttonRemove = tk.Canvas(background= colors["darkerBlue"],highlightthickness=0)
-textButtonRemove = Label(text="remove",font=font.Font(family="Times New Roman", size=16, weight="bold"),background=colors["lightnessBlue"],foreground='white')
+containerButtonPath = tk.Canvas(root,bg=colors["darkerBlue"],highlightthickness=0)
+containerButtonPath.grid(row=0,column=1,padx=10,pady=10,sticky='nswe')
 
+containerInputPath = tk.Canvas(root,bg=colors["white"],highlightthickness=0)
+containerInputPath.grid(row=0,column=2,columnspan=4,padx=10,pady=10,sticky='nswe')
 
+containerBottom = tk.Canvas(root, bg=colors["darkerBlue"], highlightthickness=0)
+containerBottom.grid(row=1,column=0,rowspan=13,columnspan=6,sticky='nswe')
 
-#position of buttons
-uploadButton.grid(row=0,column=0,padx=10,pady=10)
-textUploadButton.grid(row=0,column=0)
-buttonPath.grid(row=0,column=1,padx=10,pady=10)
-textButtonPath.grid(row=0,column=1)
-buttonAdd.grid(row=10,column=0,rowspan = 2,columnspan = 2,padx=5,pady=5,sticky='nswe')
-textButtonAdd.grid(row=10, column=0, rowspan=2, columnspan=2)
-buttonRemove.grid(row=10, column=3, rowspan=2, padx=5, pady=5, sticky='nswe')
-textButtonRemove.grid(row=10, column=3, rowspan=2)
+containerSelectHandle = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerSelectHandle.grid(row=1,column=0,rowspan=3,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
+containerSelectTopOrigin = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerSelectTopOrigin.grid(row=4,column=0,rowspan=3,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
+containerSelectBottomOrigin = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerSelectBottomOrigin.grid(row=7,column=0,rowspan=3,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
+containerButtonAdd = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerButtonAdd.grid(row=10,column=0,rowspan=4,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
 
+containerMoveHandle = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerMoveHandle.grid(row=1,column=2,rowspan=13,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
+containerAddedHandle = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerAddedHandle.grid(row=1,column=4,rowspan=9,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
 
+containerButtonRemove = tk.Canvas(root,bg=colors["lightnessBlue"],highlightthickness=0)
+containerButtonRemove.grid(row=10,column=4,rowspan=4,columnspan=2,
+                           padx=5,pady=5,sticky='nswe')
 
-#rounded corners buttons
-root.update()
-canvas_rounded_corners(8,uploadButton.winfo_width(),uploadButton.winfo_height(),colors["darkerBlue"], uploadButton)
-canvas_rounded_corners(8,buttonPath.winfo_width(),buttonPath.winfo_height(),colors["darkerBlue"], buttonPath)
-canvas_rounded_corners(8,buttonAdd.winfo_width(),buttonAdd.winfo_height(),colors["lightnessBlue"], buttonAdd)
-canvas_rounded_corners(8,buttonRemove.winfo_width(),buttonRemove.winfo_height(),colors["lightnessBlue"], buttonRemove)
+#labels
 
-#inputtext
-inputPath = Entry(root,highlightthickness=0,borderwidth=0)
-containerInputPath = tkinter.Canvas(background= colors["grey"],highlightthickness=0)
-inputSelectHandle = Entry(root,highlightthickness=0,borderwidth=0)
-containerInputSelectHandle = tkinter.Canvas(background= colors["lightnessBlue"],highlightthickness=0)
-inputSelectTopOrigin = Entry(root,highlightthickness=0,borderwidth=0)
-containerinputSelectTopOrigin = tkinter.Canvas(background= colors["lightnessBlue"],highlightthickness=0)
+labelButtonUpload = tk.Label(root,text="upload",font=fonts["buttonTopContainer"],foreground="white",bg=colors["darkerBlue"])
+labelButtonUpload.grid(row=0,column=0)
+labelButtonPath = tk.Label(root,text="path",font=fonts["buttonTopContainer"],foreground="white",bg=colors["darkerBlue"])
+labelButtonPath.grid(row=0,column=1)
 
-inputSelectBottomOrigin = Entry(root,highlightthickness=0,borderwidth=0)
-containerinputSelectBottomOrigin = tkinter.Canvas(background= colors["lightnessBlue"],highlightthickness=0)
-inputMove = Entry(root,highlightthickness=0,borderwidth=0)
-containerMove = tkinter.Canvas(background= colors["lightnessBlue"],highlightthickness=0)
-inputAddedObjects = Entry(root,highlightthickness=0,borderwidth=0)
-containerinputAddedObjects = tkinter.Canvas(background= colors["lightnessBlue"],highlightthickness=0)
+labelContainerSelectHandle = tk.Label(root,text="select handle",font=fonts["bottomContainerTitle"],foreground="white",bg=colors["lightnessBlue"])
+labelContainerSelectHandle.grid(row=1,column=0,rowspan=2,columnspan=2,padx=15,pady=15,sticky='nw')
+labelContainerSelectTopOrigin = tk.Label(root,text="select top origin",font=fonts["bottomContainerTitle"],foreground="white",bg=colors["lightnessBlue"])
+labelContainerSelectTopOrigin.grid(row=4,column=0,rowspan=2,columnspan=2,padx=15,pady=15,sticky='nw')
+labelContainerSelectBottomOrigin = tk.Label(root,text="select bottom origin",font=fonts["bottomContainerTitle"],foreground="white",bg=colors["lightnessBlue"])
+labelContainerSelectBottomOrigin.grid(row=7,column=0,rowspan=2,columnspan=2,padx=15,pady=15,sticky='nw')
+labelContainerMoveHandle = tk.Label(root,text="move handle",font=fonts["bottomContainerTitle"],foreground="white",bg=colors["lightnessBlue"])
+labelContainerMoveHandle.grid(row=1,column=2,rowspan=2,columnspan=2,padx=15,pady=15,sticky='nw')
+labelContainerListAddedObjects = tk.Label(root,text="added objects",font=fonts["bottomContainerTitle"],foreground="white",bg=colors["lightnessBlue"])
+labelContainerListAddedObjects.grid(row=1,column=4,rowspan=2,columnspan=2,padx=15,pady=15,sticky='nw')
 
-#inputtext grid
-containerInputPath.grid(row=0,column=2,columnspan=2,pady=10,padx=10, sticky='nswe')
-inputPath.grid(row=0,column=2,columnspan=2,pady=15,padx=20, sticky='nswe')
-inputPath.lift()
-
-inputSelectHandle.grid(row=2,column=0,rowspan=2,pady =10,sticky="we",padx=15, columnspan=2)
-
-
-#rounded corners inputText
-root.update()
-canvas_rounded_corners(4,containerInputPath.winfo_width(),containerInputPath.winfo_height(),"white", containerInputPath)
-
-#label containers
-labelHandleContainer = Label(text="select handle",font=fontContainer,foreground='white' , background=colors["lightnessBlue"])
-labelHandleContainer.grid(row=1,column=0,columnspan=2,rowspan=2,padx=10,sticky='we')
-labelContainerTopOrigin = Label(text="select top origin",font=fontContainer,foreground='white' , background=colors["lightnessBlue"])
-labelContainerTopOrigin.grid(row=4,column=0,columnspan=2,rowspan=2,padx=10,sticky='we')
-labelContainerBottomOrigin = Label(text="select bottom origin",font=fontContainer,foreground='white' , background=colors["lightnessBlue"])
-labelContainerBottomOrigin.grid(row=7,column=0,columnspan=2,rowspan=2,padx=10,sticky='we')
-labelContainerMove = Label(text="move",font=fontContainer,foreground='white' , background=colors["lightnessBlue"])
-labelContainerMove.grid(row=1,column=2,rowspan=2,padx=10,sticky='we')
-labelContainerAddedObjects = Label(text="objects",font=fontContainer,foreground='white' , background=colors["lightnessBlue"])
-labelContainerAddedObjects.grid(row=1,column=3,rowspan=2,padx=10,sticky='we')
-
-
-
-
-#events buttons
-uploadButton.bind("<ButtonRelease-1>",lambda event: print("Upload"))
-textUploadButton.bind("<ButtonRelease-1>",lambda event: print("Upload"))
-
-buttonPath.bind("<ButtonRelease-1>", lambda event: print("path")  )
-textButtonPath.bind("<ButtonRelease-1>",lambda event: print("path") )
-
-def upload_button_motion (e):
-    uploadButton.config(cursor="hand2")
-    textUploadButton.config( cursor="hand2")
-
-def upload_button_leave(e):
-    uploadButton.config(background=colors["darkerBlue"],cursor="arrow")
-    textUploadButton.config(cursor="arrow")
-
-def path_button_motion (e):
-    buttonPath.config(background=colors["grey"],cursor="hand2")
-    textButtonPath.config( cursor="hand2")
-def path_button_leave(e):
-    buttonPath.config(background=colors["darkerBlue"],cursor="arrow")
-    textButtonPath.config(cursor="arrow")
-
-uploadButton.bind("<Motion>", upload_button_motion  )
-textUploadButton.bind("<Motion>",upload_button_motion )
-
-uploadButton.bind("<Leave>",upload_button_leave )
-textUploadButton.bind("<Leave>",upload_button_leave )
-
-buttonPath.bind("<Motion>", path_button_motion  )
-textButtonPath.bind("<Motion>", path_button_motion  )
-
-buttonPath.bind("<Leave>",path_button_leave)
-textButtonPath.bind("<Leave>",path_button_leave)
-
-
-
-
+labelButtonButtonAdd = tk.Label(root,text="add",font=fonts["bottomButtonFont"],foreground="white",bg=colors["lightnessBlue"])
+labelButtonButtonAdd.grid(row=10,column=0,rowspan=4,columnspan=2,padx=15,pady=15)
+labelButtonButtonRemove = tk.Label(root,text="remove",font=fonts["bottomButtonFont"],foreground="white",bg=colors["lightnessBlue"])
+labelButtonButtonRemove.grid(row=10,column=4,columnspan=2,rowspan=4,padx=15,pady=15)
 
 
 root.mainloop()
-
-
